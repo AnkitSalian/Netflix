d21dc47e249ea0b7525419acf820bbc9ae577e17
Ankit Salian <ankitsalian9@gmail.com>
2020-12-26 17:47:40 +0530

    Creating Netflix jumbotron component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
